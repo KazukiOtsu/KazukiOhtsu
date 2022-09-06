cdad50227623e04abce1afa7376a683384303233
EC2 Default User <ec2-user@ip-172-31-81-81.ec2.internal>
2022-09-06 00:40:40 +0000

    実践課題のストップウォッチです

---
+++ main.js
@@ -0,0 +1,50 @@
+const stopWatch = document.getElementById("stopWatch")
+const start = document.getElementById("start")
+const stop = document.getElementById("stop")
+const reset = document.getElementById("reset")
+
+let sec = 0;
+let min = 0;
+let hour = 0;
+
+let countUp = function() {
+    sec++;
+    if(sec >= 60) {
+        min++;
+        sec = 0;
+    }
+    if(min >= 60) {
+        hour++;
+        min = 0;
+    }
+    stopWatch.innerHTML = ("00" + hour).slice(-2) + ":" + ("00" + min).slice(-2) + ":" + ("00" + sec).slice(-2);
+};
+
+let interval;
+
+function startTimer() {
+    interval = setInterval(countUp, 1000);
+    start.disabled = true;
+    stop.disabled = false;
+    reset.disabled = true;
+}
+
+function stopTimer() {
+    clearInterval(interval);
+    start.disabled = false;
+    stop.disabled = true;
+    reset.disabled = false;
+}
+
+function resetTimer() {
+    hour = 0;
+    min = 0;
+    sec = 0;
+    start.disabled = false;
+    stop.disabled = true;
+    reset.disabled = true;
+    stopWatch.innerHTML = ("00:00:00")
+}
+
+
+
