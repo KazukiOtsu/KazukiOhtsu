f5335069b58ef94d9ee04ad16bedd75e861b280b
EC2 Default User <ec2-user@ip-172-31-81-81.ec2.internal>
2022-09-06 01:42:12 +0000

    ミリ秒単位で表示されるように修正

--- main.js
+++ main.js
@@ -3,12 +3,17 @@ const start = document.getElementById("start")
 const stop = document.getElementById("stop")
 const reset = document.getElementById("reset")
 
+let minsec = 0;
 let sec = 0;
 let min = 0;
 let hour = 0;
 
 let countUp = function() {
-    sec++;
+    minsec ++
+    if(minsec >= 100) {
+        sec++;
+        minsec = 0;
+    }
     if(sec >= 60) {
         min++;
         sec = 0;
@@ -17,13 +22,13 @@ let countUp = function() {
         hour++;
         min = 0;
     }
-    stopWatch.innerHTML = ("00" + hour).slice(-2) + ":" + ("00" + min).slice(-2) + ":" + ("00" + sec).slice(-2);
+    stopWatch.innerHTML = ("00" + hour).slice(-2) + ":" + ("00" + min).slice(-2) + ":" + ("00" + sec).slice(-2) + ":" + ("00" + minsec).slice(-2);
 };
 
 let interval;
 
 function startTimer() {
-    interval = setInterval(countUp, 1000);
+    interval = setInterval(countUp, 10);
     start.disabled = true;
     stop.disabled = false;
     reset.disabled = true;
@@ -43,7 +48,7 @@ function resetTimer() {
     start.disabled = false;
     stop.disabled = true;
     reset.disabled = true;
-    stopWatch.innerHTML = ("00:00:00")
+    stopWatch.innerHTML = ("00:00:00:00")
 }
 
 
